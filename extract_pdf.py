dfe4984ca80f54113ad29591181b279965821425
Omkar-ratzar <swamiomkar2004@gmail.com>
2026-05-14 22:29:03 +0530

    first commit

---
+++ extract_pdf.py
@@ -0,0 +1,15 @@
+#for pdfs
+import fitz  # PyMuPDF
+# from app.core.log import logger
+# from app.core.errors import safe_execution
+# from app.db.file_repo import mark_processed
+# from app.core.utils import normalize_path
+
+# @safe_execution(component="EXTRACTOR",log_args=True)
+def extract_pdf(path):
+    doc = fitz.open(path)
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    # logger.info("PDF has been extracted. Path:"+path)
+    return text
---
+++ readme.md
@@ -0,0 +1,11 @@
+A simple project to perform
+
+Data extraction,
+Data parsing,
+Data cleaning.
+
+Upload resume, get output in dataframes.
+
+DF cols:
+
+Caandidate_Name, Candidate_mail, candidate_phone_number, candidate_address, Candidate_education
